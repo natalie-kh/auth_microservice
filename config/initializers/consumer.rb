b67d06880bc31519afd50d837af2fca776d8d5cb
Natalya Bogdanova <natalya.bogdanova@karuna.group>
2021-05-07 18:05:52 +0300

    rabbit-mq

---
+++ config/initializers/consumer.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+channel = RabbitMq.consumer_channel
+exchange = channel.default_exchange
+queue = channel.queue('auth', durable: true)
+
+queue.subscribe(manual_ack: true) do |delivery_info, properties, payload|
+  payload = JSON(payload)
+  uuid = begin
+    JwtEncoder.decode(payload['token'])['uuid']
+  rescue StandardError
+    {}
+  end
+
+  result = Auth::FetchUserService.call(uuid)
+
+  responce =
+    if result.success?
+      { user_id: result.user.id }
+    else
+      {}
+    end
+
+  exchange.publish(
+    responce.to_json,
+    routing_key: properties.reply_to,
+    correlation_id: properties.correlation_id
+  )
+
+  channel.ack(delivery_info.delivery_tag)
+end
